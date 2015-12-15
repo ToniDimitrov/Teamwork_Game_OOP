811ed753b7e013e302fefd2121b80022aa430ead
Twist007 <h_bratoev@abv.bg>
2015-12-15 10:56:07 +0200

    InitItemImage

--- Game/Models/Item.cs
+++ Game/Models/Item.cs
@@ -1,22 +1,44 @@
 ﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Game.Interfaces;
 using Game.Models;
 
 namespace Game.Models
 {
-    public abstract class Item : GameObject
+    public abstract class Item : GameObject, IItem
     {
         private int attackEffect;
         private int defenceEffect;
         private int healthEffect;
 
-        protected Item(string id, Point location, Size objectSize, int attackEffect, int defenceEffect,
-            int healthEffect) : base(id, location, objectSize)
+        public PictureBox ItemImage { get; set; }
+
+        private string pathImage;
+        
+        protected Item(string id, Point location, Size objectSize, int attackEffect, int defenceEffect, int healthEffect, string pathImage)
+            : base(id, location, objectSize)
         {
             this.AttackEffect = attackEffect;
             this.DefenceEffect = defenceEffect;
             this.HealthEffect = healthEffect;
+            this.pathImage = pathImage;
+
+            InitItemImage();
         }
 
+        public void InitItemImage()
+        {
+            this.ItemImage = new PictureBox
+            {
+                Image = Image.FromFile(this.pathImage),
+                Size = new System.Drawing.Size(this.ObjectSize.Width, this.ObjectSize.Height),
+                Location = new System.Drawing.Point(this.Location.X, this.Location.Y),
+                Enabled = true,
+                Visible = true,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+        }
         public int AttackEffect
         {
             get { return this.attackEffect; }
